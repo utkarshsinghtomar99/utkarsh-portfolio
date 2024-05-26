1d8c502c526b321eee08779b304d22aad531e057
Utkarsh Singh Tomar <utomar94@gmail.com>
2024-05-26 11:17:05 +0530

    Update authService.js

--- client/src/redux/auth/authService.js
+++ client/src/redux/auth/authService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const loginAdmin = async (formData) => {
   const res = await axios.post(
-    "http://localhost:5000/api/portfolio-auth/admin-login",
+    "/api/portfolio-auth/admin-login",
     formData
   );
   localStorage.setItem("admin", JSON.stringify(res.data.data.username));
