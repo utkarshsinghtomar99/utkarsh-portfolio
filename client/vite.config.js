1ff74ce84718c2446b6dc3c944432a94ebea573f
Utkarsh Singh Tomar <utomar94@gmail.com>
2024-05-26 11:11:34 +0530

    Update vite.config.js setup proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://utkarsh-portfolio-4ijs.onrender.com/',
+        changeOrigin: true,
+      }
+    }
+  }
 })
